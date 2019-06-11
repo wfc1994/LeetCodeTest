82c346dcc6c919b096b750534157ca781ed0fbab
老王 <779481237@qq.com>
2019-06-12 00:23:07 +0800

    improve

--- src/Solution_1021.java
+++ src/Solution_1021.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Solution_1021 {
     public String removeOuterParentheses(String S) {
 //        while(S.contains("()")){
@@ -5,15 +7,41 @@ public class Solution_1021 {
 //        }
         System.out.println(S.indexOf("()"));
         char [] sa = S.toCharArray();
+        int [] san = new int[sa.length];
         for(int i=0;i<sa.length;i++){
-            if(sa[i]=='('&&sa[i+1]==')'){
-                sa[i]='0';
-                sa[i+1]='0';
+            if(sa[i]=='('){
+                san[i]=-1;
+            }
+            if(sa[i]==')'){
+                san[i]=1;
             }
 
         }
-        for(int i=0;i<sa.length;i++){
+        for(int i=0;i<san.length;i++){
             System.out.print(sa[i]+" ");
+        }
+        System.out.println();
+        for(int i=0;i<san.length;i++){
+            System.out.print(san[i]+" ");
+        }
+
+        int sum=0;
+        ArrayList<Integer> removeindexlist = new ArrayList<>();
+        for(int i=0;i<san.length;i++){
+            sum = sum + san[i];
+            if(sum==0){
+                removeindexlist.add(i);
+            }
+        }
+        for(int i=san.length-1;i>=0;i--){
+            sum = sum + san[i];
+            if(sum==0){
+                removeindexlist.add(i);
+            }
+        }
+        System.out.println();
+        for(int i: removeindexlist){
+            System.out.print(i+" ");
         }
         return null;
     }
