354b2f6f9aaa7b192c0ff78af67f46e46965bd0a
老王 <779481237@qq.com>
2019-06-13 11:44:37 +0800

    improve

--- src/Solution_1021.java
+++ src/Solution_1021.java
@@ -2,10 +2,7 @@ import java.util.ArrayList;
 
 public class Solution_1021 {
     public String removeOuterParentheses(String S) {
-//        while(S.contains("()")){
-//
-//        }
-        System.out.println(S.indexOf("()"));
+
         char [] sa = S.toCharArray();
         int [] san = new int[sa.length];
         for(int i=0;i<sa.length;i++){
@@ -15,15 +12,8 @@ public class Solution_1021 {
             if(sa[i]==')'){
                 san[i]=1;
             }
+        }
 
-        }
-        for(int i=0;i<san.length;i++){
-            System.out.print(sa[i]+" ");
-        }
-        System.out.println();
-        for(int i=0;i<san.length;i++){
-            System.out.print(san[i]+" ");
-        }
 
         int sum=0;
         ArrayList<Integer> removeindexlist = new ArrayList<>();
@@ -39,15 +29,19 @@ public class Solution_1021 {
                 removeindexlist.add(i);
             }
         }
-        System.out.println();
-        for(int i: removeindexlist){
-            System.out.print(i+" ");
+
+        String result = "";
+        for(int i=0;i<sa.length;i++){
+            if(!removeindexlist.contains(i)){
+                result+=sa[i];
+            }
         }
-        return null;
+
+        return result;
     }
 
     public static void main(String[] args){
-        String s = "(()())(())(()(()))";
+        String s = "()()";
         Solution_1021 solution = new Solution_1021();
         solution.removeOuterParentheses(s);
 
