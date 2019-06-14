427e08378ee6f0a9de00fe129d830540d0403dc7
wfc <779481237@qq.com>
2019-06-14 11:16:56 +0800

    1021号题的优化

---
+++ src/Solution_1021_1.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Solution_1021_1 {
+    public String removeOuterParentheses(String S) {
+
+        StringBuilder result = new StringBuilder();
+        int count = 0;
+        char[] inputs = S.toCharArray();
+        for (int i = 0; i < inputs.length; i++) {
+            char currentChar = inputs[i];
+            if (currentChar == '(') {
+                if (count++ > 0) result.append(currentChar);
+            } else {
+                if (--count > 0) result.append(currentChar);
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args){
+        String s = "(())()";
+        Solution_1021_1 solution = new Solution_1021_1();
+        solution.removeOuterParentheses(s);
+
+    }
+}
