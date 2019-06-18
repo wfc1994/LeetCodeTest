0d5e4a13df497515d4d3095ea78e856d55391f1f
王飞程 <779481237@qq.com>
2019-06-18 20:38:24 +0800

    977，考虑从首尾做index

--- src/Solution_977.java
+++ src/Solution_977.java
@@ -1,49 +1,79 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Solution_977 {
     public int[] sortedSquares(int[] A) {
+        //找到两个大于等于0的第一个位置和小于等于0的位置
+        int pindex = 0;
+        int nindex = 0;
         int[] result = new int[A.length];
-        LinkedList<Integer> s = new LinkedList();
-        LinkedList<Integer> p = new LinkedList();
-
         for(int i=0;i<A.length;i++){
-            if(A[i]>=0){
-                s.add(A[i]);
-            }else {
-                p.add(A[i]);
+            if(A[i]>=0){//找到临界位置
+                pindex = i;
+                nindex = i-1;
+                break;
             }
         }
-        int temp = 0;
 
-        if(s.size()==0){
-            temp = -p.getLast();
-            s.add(temp);
-        }
-
-
-        int index = 0;
-        while (p.size()!=0){
-            temp = -p.getLast();
-            for(int i=index;i<s.size();i++){
-                if(temp<s.get(i)){
-                    s.add(i,temp);
-                    index = i;
+        if(nindex==-1){
+            for(int i=0;i<A.length;i++){
+                result[i] = A[i] * A[i];
+            }
+        }else if(pindex==nindex){
+            int count = 0;
+            for(int i=A.length-1;i>=0;i--){
+                result[count] = A[i] * A[i];
+                count++;
+            }
+        }else {
+            int count=0;
+            while (true){
+                //循环终止两个条件，要么pindex到末尾，要么nindex到0位
+                if(nindex<0){
+                    while(pindex<=A.length-1){
+                        System.out.println(pindex);
+                        result[count] = A[pindex]*A[pindex];
+                        count++;
+                        pindex++;
+                    }
                     break;
                 }
+                if(pindex>A.length-1){
+                    while(nindex>=0){
+                        System.out.println(nindex);
+                        result[count] = A[nindex]*A[nindex];
+                        count++;
+                        nindex--;
+                    }
+                    break;
+                }
+
+                if(-A[nindex]<A[pindex]){
+                    System.out.println(nindex);
+                    result[count] = A[nindex]*A[nindex];
+                    count++;
+                    nindex--;
+                }else{
+                    System.out.println(pindex);
+                    result[count] = A[pindex]*A[pindex];
+                    count++;
+                    pindex++;
+                }
+
+
             }
-            p.removeLast();
         }
 
-        for(int i=0;i<s.size();i++){
-            result[i] = s.get(i)*s.get(i);
-            System.out.println(result[i]);
-        }
+//        for(int i=0;i<result.length;i++){
+//            System.out.print(result[i]+" ");
+//        }
 
         return result;
     }
 
     public static void main(String[] args){
-        int[] A = {-2,-1};
+        int[] A = {3,4};
         Solution_977 solution = new Solution_977();
         solution.sortedSquares(A);
 
---
+++ src/Solution_977_1.java
@@ -0,0 +1,27 @@
+public class Solution_977_1 {
+    public int[] sortedSquares(int[] A) {
+        int res[]=new int[A.length];
+        int count=A.length-1;
+        int right=A.length-1;
+        int left=0;
+        while(left<=right){
+            if(A[left]*A[left]<=A[right]*A[right]){
+                res[count]=A[right]*A[right];
+                right--;
+                count--;
+            }else{
+                res[count]=A[left]*A[left];
+                left++;
+                count--;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args){
+        int[] A = {3,4};
+        Solution_977_1 solution = new Solution_977_1();
+        solution.sortedSquares(A);
+
+    }
+}
